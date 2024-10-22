850a3a48be57b241285df7e7fcac1257571db9d5
Ellenkka <643221@csinow.edu>
2024-10-22 22:51:04 +0000

    Testing Tue Oct 22 2024 22:51:04 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -1,7 +1,19 @@
 # Write your code here
-""" This file converts a user's input into an integer """
-whole_num = int(input())
-print ("_" + 10)
-print (whole_num)
-print ((str(whole_num) * 2) * 5)
-print ("_" + 10)
+"""
+This script prints the multiplication table of a whole number
+by the multiples of 2, from 1 to 10.
+"""
+
+# Get user input and cast it to an integer
+whole_num = int(input("Enter a whole number: "))
+
+# Print the top border
+print("_" * 10)
+
+# Print the multiplication table
+for i in range(1, 11):
+    result = whole_num * (i * 2)
+    print(f"{whole_num} * {i * 2} = {result}")
+
+# Print the bottom border
+print("_" * 10)
