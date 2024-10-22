5156944e34ef25c421ffa9acc0ff41d3b9a75f2a
Ellenkka <643221@csinow.edu>
2024-10-22 01:44:54 +0000

    Testing Tue Oct 22 2024 01:44:54 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex03A/student/main.py
+++ chapter1/ex03A/student/main.py
@@ -1,1 +1,15 @@
 # Write your code here
+distance_in_miles = 60
+time_in_hours = 3
+
+distanceInKnots = distance_in_miles/1.15078
+distanceInFeet = distance_in_miles*5280
+timeInSeconds = time_in_hours * 3600
+speedInKnots = distanceInKnots/time_in_hours
+speedInMilesPerHour = distance_in_miles/time_in_hours
+speedInFeetPerSecond = distanceInFeet/timeInSeconds
+
+print ("The speed in feet per second is " + str(speedInFeetPerSecond))
+print("The speed in knots is: " + str(speedInKnots))
+print("the speed in miles per hour is " + str(speedInMilesPerHour))
+
