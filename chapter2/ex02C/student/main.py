b70b16eb52fbe49d5fe06131a8e8024c99fde338
Ellenkka <643221@csinow.edu>
2024-10-23 01:54:48 +0000

    Testing Wed Oct 23 2024 01:54:48 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02C/student/main.py
+++ chapter2/ex02C/student/main.py
@@ -1,1 +1,8 @@
 # Write your code here
+
+mySentence = input("Type a sentence: ")
+myWord= input("Word to look for in sentence: ")
+mySubString = mySentence.strip()
+mySubString = mySubString.lower()
+myCount=mySubString.count(myWord)
+print("There are", myCount, "occurences of", repr(myWord),"in the sentence.")
