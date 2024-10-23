f9f51d31a0c53ed9bc2d753a6f5779c231ae0738
Ellenkka <643221@csinow.edu>
2024-10-23 01:18:58 +0000

    Testing Wed Oct 23 2024 01:18:58 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02B/student/main.py
+++ chapter2/ex02B/student/main.py
@@ -1,1 +1,13 @@
 # Write your code here
+
+myString = input("Word to convert: ")
+count = int(input("How many letters at the end of the word should be converted? "))
+strStart = 0
+strEnd = len(myString)
+secStart =strEnd-count
+str1=myString[strStart:secStart]
+str2=myString[secStart:strEnd]
+secCount=strEnd-count
+strSect = myString[secCount:strEnd]
+strWhole = str1 + str2.upper()
+print(strWhole)
