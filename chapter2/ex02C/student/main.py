da3cd3118fcb076a3295cb953c6da8ef84b9224c
Ellenkka <643221@csinow.edu>
2024-10-23 02:25:25 +0000

    Testing Wed Oct 23 2024 02:25:25 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02C/student/main.py
+++ chapter2/ex02C/student/main.py
@@ -1,7 +1,8 @@
-# write your code here
+# Write your code here
+
 mySentence = input("Type a sentence: ")
 myWord = input("Word to look for in sentence: ")
-# Normalize both the sentence and the word to lowercase
+#Normalize both the sentence and the word to lowercase
 mySubString = mySentence.strip().lower()
 myWord = myWord.lower()
 # Count occurrences of the word
