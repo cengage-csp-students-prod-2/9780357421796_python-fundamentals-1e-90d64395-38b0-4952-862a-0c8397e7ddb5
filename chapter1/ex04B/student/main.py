cf952bb88b4d405bfcf0b11d2f9cb2121f5b55ee
Ellenkka <643221@csinow.edu>
2024-10-23 00:25:00 +0000

    Testing Wed Oct 23 2024 00:25:00 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -6,8 +6,8 @@ whole_num = int(input("Enter a whole number: "))
 
 print("_" * 10)
 
-result = whole_num * 10
-print(result)
+for i in range(1, 11):
+    result = whole_num * (i * 2)
+    print(f"{whole_num} * {i * 2} = {result}")
 
 print("_" * 10)
-
--- chapter2/ex01B/student/main.py
+++ chapter2/ex01B/student/main.py
@@ -1,1 +1,18 @@
 # Write your code here
+
+noDays = int(input("Days you've been driving: "))
+
+noYears = int(noDays/365)
+
+noWeeks = int(noDays%365/7)
+
+noDay = int(noDays%365%7)
+
+print("You've been driving for: ")
+
+print("Years:", noYears)
+
+print("Weeks:", noWeeks)
+
+print("Days:", noDay)
+
