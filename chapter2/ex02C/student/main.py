0c5c56db70a2661bfb2bbac916ad4cac6aa100b7
Ellenkka <643221@csinow.edu>
2024-10-23 02:26:01 +0000

    Testing Wed Oct 23 2024 02:26:01 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02C/student/main.py
+++ chapter2/ex02C/student/main.py
@@ -5,6 +5,6 @@ myWord = input("Word to look for in sentence: ")
 #Normalize both the sentence and the word to lowercase
 mySubString = mySentence.strip().lower()
 myWord = myWord.lower()
-# Count occurrences of the word
+#Count occurrences of the word
 myCount = mySubString.split().count(myWord)
 print("There are", myCount, "occurrences of", repr(myWord), "in the sentence.")
