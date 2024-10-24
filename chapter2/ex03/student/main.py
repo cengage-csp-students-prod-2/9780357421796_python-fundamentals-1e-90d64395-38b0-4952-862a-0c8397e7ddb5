1d4ee98f285bd18cdfcf2f2e5c31b6dc76371f61
Ellenkka <643221@csinow.edu>
2024-10-24 01:39:45 +0000

    Testing Thu Oct 24 2024 01:39:45 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/main.py
+++ chapter2/ex03/student/main.py
@@ -1,1 +1,6 @@
 # Write your code here
+myNumbers = [34, 9, 32, 91, 58, 13, 77, 21, 56]
+print(myNumbers)
+myNumber = input("How many numbers do you want? ")
+myIntNumber = int(myNumber)
+print(myNumbers[:myIntNumber])
