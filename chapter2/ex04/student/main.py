c324c1c43486b643afee34ef9cf3e850de1d870f
Ellenkka <643221@csinow.edu>
2024-10-24 01:48:22 +0000

    Testing Thu Oct 24 2024 01:48:22 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex04/student/main.py
+++ chapter2/ex04/student/main.py
@@ -7,6 +7,7 @@ days = 32
 if days > 31:
 	print("Not a valid day of the month.")
 
+
 character = "c"
 if character not in ["a", "i"]:
     print(f"The letter '{character}' does not also constitute a word.")
