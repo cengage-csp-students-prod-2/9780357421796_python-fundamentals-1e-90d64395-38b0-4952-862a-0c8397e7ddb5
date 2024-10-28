715970c62bf549d2672afc400f338025131b2971
Ellenkka <643221@csinow.edu>
2024-10-28 22:28:47 +0000

    Testing Mon Oct 28 2024 22:28:47 GMT+0000 (Coordinated Universal Time)

--- chapter3/pe02/student/main.py
+++ chapter3/pe02/student/main.py
@@ -1,1 +1,12 @@
 # Write your code here
+release_year = '1991'
+answer = input('When was Python first released? ')
+
+if answer == release_year:
+	print('Congratulations! That is correct.')
+elif answer > release_year:
+	print('No, that\'s too late')
+elif answer < release_year:
+  print('No, that\'s too early')
+else
+  print('Bye!')
