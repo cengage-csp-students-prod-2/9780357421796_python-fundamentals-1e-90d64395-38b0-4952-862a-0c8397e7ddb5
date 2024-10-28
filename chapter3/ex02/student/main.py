02cc572db6745eeb657dc3438def2013fec202f5
Ellenkka <643221@csinow.edu>
2024-10-28 22:34:10 +0000

    Testing Mon Oct 28 2024 22:34:10 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/main.py
+++ chapter3/ex02/student/main.py
@@ -1,10 +1,21 @@
-python3_year = 2008
-year = int(input('When was Python3 first released?'))
+#python3_year = 2008
+#year = int(input('When was Python3 first released?'))
 
-if year == python3_year:
-	print(bool(python3_year))
-	print('Correct')
+#if year == python3_year:
+#	print(bool(python3_year))
+#	print('Correct')
+#else:
+#	print(False)
+#	print('Incorrect')
+#	print('Have a great day!')
+
+answer = (input('Return True or False: Python3 was released in 2008: '))
+answer = answer.capitalize()
+if answer == 'True':
+	print('Correct.')
+elif answer == 'False':
+    print('Incorrect.')
 else:
-	print(False)
-	print('Incorrect')
-	print('Have a great day!')
+	print('Please answer True or False')
+
+print("Have a great day!")
