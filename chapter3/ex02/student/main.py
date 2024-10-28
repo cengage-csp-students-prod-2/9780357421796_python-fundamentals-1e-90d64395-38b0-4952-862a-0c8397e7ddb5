41e76f9cb694b7a8cd51b25f397c1efcc84d193e
Ellenkka <643221@csinow.edu>
2024-10-28 22:36:08 +0000

    Testing Mon Oct 28 2024 22:36:08 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/main.py
+++ chapter3/ex02/student/main.py
@@ -11,10 +11,12 @@
 
 answer = (input('Return True or False: Python3 was released in 2008: '))
 answer = answer.capitalize()
+
 if answer == 'True':
 	print('Correct.')
 elif answer == 'False':
     print('Incorrect.')
 else:
 	print('Please answer True or False')
+	
 print("Have a great day!")
