2732317028cb2d3e219475d60127fc6510930997
Ellenkka <643221@csinow.edu>
2024-10-28 21:50:08 +0000

    Testing Mon Oct 28 2024 21:50:08 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -3,11 +3,8 @@
 This is how we create a multiplication table
 """
 whole_num = int(input("Enter a whole number: "))
-
 print("_" * 10)
-
 for i in range(1, 11):
     result = whole_num * (i * 2)
     print(f"{whole_num} * {i * 2} = {result}")
-
 print("_" * 10)
