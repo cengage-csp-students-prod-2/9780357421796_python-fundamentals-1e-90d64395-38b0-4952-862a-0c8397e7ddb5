740f90241d3371f57b447805f2ddab06d0eba183
Ellenkka <643221@csinow.edu>
2024-10-29 00:48:08 +0000

    Testing Tue Oct 29 2024 00:48:08 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex03/student/main.py
+++ chapter3/ex03/student/main.py
@@ -1,1 +1,23 @@
 # Write your code here
+#name = input("Enter your name?")
+#password = input("Enter your password")
+
+#while name == "Josh":
+    #if password == "Pas$Word":
+    #print("Welcome back, Josh")
+#else:
+#    print("Incorrect password, try again...")
+
+# main.py
+
+name = input("Enter your name: ")
+password = "Pas$Word"
+first = True
+
+while True:
+    user_input = input("Enter your password: ")
+    if user_input == password:
+        print(f"Welcome back, {name}")
+        break
+    else:
+        print("Incorrect password, try again...")
