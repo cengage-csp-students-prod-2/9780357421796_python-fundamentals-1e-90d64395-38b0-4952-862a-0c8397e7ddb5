10f9f20e5d13a040c3975a4c60934fdcbb4046f4
Ellenkka <643221@csinow.edu>
2024-10-29 01:05:17 +0000

    Testing Tue Oct 29 2024 01:05:17 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -2,15 +2,8 @@
 """
 This is how we create a multiplication table
 """
-# Get user input and cast it to an integer
 whole_num = int(input("Type a whole number: "))
-
-# Print top border
 print("_" * 10)
-
-# Print the whole number and its 2's multiplication table
 for i in range(1, 11):
     print(f"{whole_num} x {i * 2} = {whole_num * (i * 2)}")
-
-# Print bottom border
 print("_" * 10)
