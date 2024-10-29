49f355a1d46c848c3626e1762bec5cf3c616eb92
Ellenkka <643221@csinow.edu>
2024-10-29 02:06:42 +0000

    Testing Tue Oct 29 2024 02:06:42 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -2,15 +2,9 @@
 """
 This is how we create a multiplication table
 """
-print("_" * 10)
+print ("_" * 10)
 whole_num = input("Type a whole number: ")
-
-try:
-    num = int(whole_num)
-    print("_" * 10)
-    print(f"Multiplication table for {num}:")
-    for i in range(1, 11):
-        print(f"{num} x {i} = {num * i}")
-    print("_" * 10)
-except ValueError:
-    print("Please enter a valid whole number.")
+print ("_" * 10)
+print (whole_num)
+print ((str(whole_num) * 2) * 5)
+print ("_" * 10)
