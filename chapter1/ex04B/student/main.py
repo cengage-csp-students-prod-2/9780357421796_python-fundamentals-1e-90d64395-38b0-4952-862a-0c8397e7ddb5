f6d053b7045c5b924db36737f05824213163d6bc
Ellenkka <643221@csinow.edu>
2024-10-29 02:18:03 +0000

    Testing Tue Oct 29 2024 02:18:03 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -4,9 +4,9 @@ This is how we create a multiplication table
 whole_num = int(input("Type a whole number: "))
 print ("_" * 10)
 print ("Number: " + whole_num)
-print (whole_num) * 2
-print (whole_num) * 4
-print (whole_num) * 6
-print (whole_num) * 8
-print (whole_num) * 10
+print (str(whole_num) * 2)
+print (str(whole_num) * 4)
+print (str(whole_num) * 6)
+print (str(whole_num) * 8)
+print (str(whole_num) * 10)
 print ("_" * 10)
