f57189140a019df52ae98ed11773b22df02b2011
Ellenkka <643221@csinow.edu>
2024-10-29 22:37:42 +0000

    Testing Tue Oct 29 2024 22:37:42 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex09/student/main.py
+++ chapter3/ex09/student/main.py
@@ -1,1 +1,12 @@
 # Write your code here
+for x in range(10,101):
+    if x == 0:
+        pass
+    if(x%5)!= 0:
+        continue
+    if type(x) != int:
+        continue
+    if x == 95:
+        break
+    else:
+        print(x)
