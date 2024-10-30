f4c9ed7920010d97097bc5bd8c7a43a46ecd73c0
Ellenkka <643221@csinow.edu>
2024-10-30 01:07:15 +0000

    Testing Wed Oct 30 2024 01:07:15 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/main.py
+++ chapter4/ex03/student/main.py
@@ -1,1 +1,8 @@
 # Write your code here
+def skip_integers(*args):
+    for arg in args:
+        if isinstance(arg, int):
+            continue
+        print(arg)
+        
+skip_integers(3, 5.2, "value", 6.0)
