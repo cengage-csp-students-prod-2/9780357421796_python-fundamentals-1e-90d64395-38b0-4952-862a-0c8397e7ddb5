522a41a43637bd107edad6394505905c1c2b219a
Ellenkka <643221@csinow.edu>
2024-10-31 00:54:39 +0000

    Testing Thu Oct 31 2024 00:54:39 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/main.py
+++ chapter5/ex06/student/main.py
@@ -1,12 +1,12 @@
 # Write your code here
 cars = ('coupe', 'coupe', 'coupe', 'carbiolet', 'sedan')
-x = cars.count('coupe')
+fav = cars.count('coupe')
 print(x)
 amt = len(cars)
 print(amt)
 
 stat = 0.45
-if x/amt > stat:
+if fav/amt > stat:
     print("Too many coupes in the garage")
     else:
     print("You are all set with cars. to the terminal.")
