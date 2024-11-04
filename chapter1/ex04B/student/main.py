638f3f7c55f3db0dc6fc6bfa68a7da46dc42e68d
Ellenkka <643221@csinow.edu>
2024-11-04 17:40:37 +0000

    Testing Mon Nov 04 2024 17:40:37 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex04B/student/main.py
+++ chapter1/ex04B/student/main.py
@@ -1,12 +1,18 @@
 """
 This is how we create a multiplication table
 """
-whole_num = int(input("Type a whole number: "))
-print ("_" * 10)
-print ("Number: " + whole_num)
-print (int(whole_num) * 2)
-print (int(whole_num) * 4)
-print (int(whole_num) * 6)
-print (int(whole_num) * 8)
-print (int(whole_num) * 10)
-print ("_" * 10)
+# Getting the user input and converting it to an integer
+whole_num = int(input("Generate a multiplication table for: "))
+
+# Printing the top border
+print("_" * 10)
+
+# Printing the first line with the original number
+print(f"Number: {whole_num}")
+
+# Printing the multiplication table for multiples of 2 till 10
+for i in range(2, 11, 2):
+    print(f"{i}: {whole_num * i}")
+
+# Printing the bottom border
+print("_" * 10)
