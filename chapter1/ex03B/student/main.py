b2743a565e987ace08c069c7e4c8562889a7a7bb
Ellenkka <643221@csinow.edu>
2024-11-04 17:49:51 +0000

    Testing Mon Nov 04 2024 17:49:51 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex03B/student/main.py
+++ chapter1/ex03B/student/main.py
@@ -1,1 +1,10 @@
 # Write your code here
+PI = 3.14159
+radius = 3
+
+diameter = 2 * radius
+
+area = PI * radius ** 2
+
+print(f"Diameter of the circle: {diameter}")
+print(f"Area of the circle: {area}")
