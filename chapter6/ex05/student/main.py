bc21b72b6fc6195639c8100e1aaec96621937b42
Ellenkka <643221@csinow.edu>
2024-11-05 23:11:12 +0000

    Testing Tue Nov 05 2024 23:11:12 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex05/student/main.py
+++ chapter6/ex05/student/main.py
@@ -1,9 +1,9 @@
 # Write your code here
-y = [2, 4, 6, 8]
-x = [3, 6, 9, 12]
+y = {2, 4, 6, 8}
+x = {3, 6, 9, 12}
 
 def unite_lists():
-    z = set(x).union(set(y))
+    z = x.union(y)
     print(z)
 
 unite_lists()
