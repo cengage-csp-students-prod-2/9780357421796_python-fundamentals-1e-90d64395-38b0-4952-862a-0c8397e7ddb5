470718f1d8f6c16de42c12ad75b3de0ae8fde67b
Ellenkka <643221@csinow.edu>
2024-11-05 23:08:34 +0000

    Testing Tue Nov 05 2024 23:08:34 GMT+0000 (Coordinated Universal Time)

--- chapter6/pe04/student/main.py
+++ chapter6/pe04/student/main.py
@@ -1,1 +1,17 @@
 # Write your code here
+v = set({"car", "bus", "train"})
+print(v)
+
+v.add("bicycle")
+print(v)
+
+for veh in v:
+    print(veh)
+
+print(v.pop())
+
+v.remove("car")
+print(v)
+
+v.clear()
+print(v)
--- chapter6/pe05/student/main.py
+++ chapter6/pe05/student/main.py
@@ -1,1 +1,14 @@
 # Write your code here
+l = {"lion", "tiger", "giraffe", "frog"}
+w = {"frog", "fish", "octopus"}
+y = l.union(w)
+print(y)
+
+z = l.intersection(w)
+print(z)
+
+x = l - w
+print(x)
+
+u = l.symmetric_difference(w)
+print(u)
