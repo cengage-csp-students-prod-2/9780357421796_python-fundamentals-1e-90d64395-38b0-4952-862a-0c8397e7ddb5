5dc4d3079f374e22ca76502f7fb6f9c15ebc6860
Ellenkka <643221@csinow.edu>
2024-11-05 01:45:34 +0000

    Testing Tue Nov 05 2024 01:45:33 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01C/student/main.py
+++ chapter6/ex01C/student/main.py
@@ -1,1 +1,10 @@
 # Write your code here
+veg1 = {0: 'Carrot', 1: 'Raddish'}
+veg2 = {2: 'Beet', 3: 'Potato'}
+
+def content_combiner (myDict1, myDict2):
+    myGardenDict = myDict1 | myDict2
+    return myGardenDict
+
+myOutput = content_combiner(veg1,veg2)
+print(myOutput)
