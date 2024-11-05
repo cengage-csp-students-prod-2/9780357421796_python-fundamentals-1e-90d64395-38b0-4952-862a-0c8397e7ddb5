952256fc95f4a3621df9cf671314fc17d7ab90c1
Ellenkka <643221@csinow.edu>
2024-11-05 01:38:04 +0000

    Testing Tue Nov 05 2024 01:38:04 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01B/student/main.py
+++ chapter6/ex01B/student/main.py
@@ -2,14 +2,18 @@
 myDict = dict()
 
 def word_counter(myStringParm):
+    myDict = dict()
+    myWord = ""
+    myCount = 0
     myStringParm = myStringParm.strip()
     myStringParm = myStringParm.lower()
+    parm_length = len(myStringParm)
     for myLet in myStringParm:
-        if myLet not in myDict:
-            #myOccur = myLet
-            myOccur=len(myStringParm.split()) - 1
-            myDict[myLet]= myOccur
+        if myLet not in myWord:
+            myWord = myWord + myLet
+            myCount = myCount + 1
+            myDict.update({myLet:myCount})
+    sorted(myDict)
     print(myDict)
-    
+
 word_counter("Mississippi")
-print(word_counter("Mississippi"))
