90d51feaea3a8c24ee32c42f305582149025b0d7
Ellenkka <643221@csinow.edu>
2024-11-05 02:28:46 +0000

    Testing Tue Nov 05 2024 02:28:46 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01B/student/main.py
+++ chapter6/ex01B/student/main.py
@@ -1,23 +1,17 @@
 # Write your code here
-myDict = dict()
-
 def word_counter(myStringParm):
     myDict = dict()
     myWord = ""
     myCount = 0
-
-myStringParm = myStringParm.strip()
-myStringParm = myStringParm.lower()
-parm_length = len(myStringParm)
-    
+    myStringParm = myStringParm.strip()
+    myStringParm = myStringParm.lower()
+    parm_length = len(myStringParm)
     for myLet in myStringParm:
         if myLet not in myWord:
             myWord = myWord + myLet
             myCount = myCount + 1
             myDict.update({myLet:myCount})
-            
-sorted(myDict)
-print(myDict)
+    sorted(myDict)
+    print(myDict)
 
 word_counter("Mississippi")
-print(word_counter("Mississippi"))
