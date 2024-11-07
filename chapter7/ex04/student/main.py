af66e560073a83074faefebda2b1427c14455e70
Ellenkka <643221@csinow.edu>
2024-11-07 02:21:13 +0000

    Testing Thu Nov 07 2024 02:21:13 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex04/student/main.py
+++ chapter7/ex04/student/main.py
@@ -1,5 +1,12 @@
 # Write your Package class here
-
+class Package:
+    items_limit = 6
+    def __init__(self, items):
+        self.items = items
+        if Package.items_limit < self.items:
+            print(f"You need to remove {Package.items_limit - self.items} item to continue.")
+        else:
+            print(f"There are {self.items} items in the package being shipped out.")
 
 # This is to test your code
 if __name__ == '__main__':
--- chapter7/pe04a/student/main.py
+++ chapter7/pe04a/student/main.py
@@ -1,1 +1,6 @@
 # Write your code here
+class WebBrowser:
+	def __init__(self, page):
+		self.history = [page]
+		self.current_page = page
+		self.is_incognito = False
