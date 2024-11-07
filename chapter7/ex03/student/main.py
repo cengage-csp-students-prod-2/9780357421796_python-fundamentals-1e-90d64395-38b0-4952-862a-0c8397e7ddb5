059bf9269869f0cda7045aa33c9c51d279e6d597
Ellenkka <643221@csinow.edu>
2024-11-07 01:30:04 +0000

    Testing Thu Nov 07 2024 01:30:04 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex03/student/main.py
+++ chapter7/ex03/student/main.py
@@ -1,7 +1,24 @@
-import math
-
 # Write your Wheel class here
 
+import math
+
+class Wheel:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def wheel_area(self):
+        area = math.pi * self.radius * self.radius
+        return area
+
+    def wheel_perimeter(self):
+        circumference = 2 * math.pi * self.radius
+        return circumference
+
+
+
+    def swap_radius(self, new_radius):
+        self.radius = new_radius
+        return new_radius
 
 # Use this to test your code
 if __name__ == "__main__":
