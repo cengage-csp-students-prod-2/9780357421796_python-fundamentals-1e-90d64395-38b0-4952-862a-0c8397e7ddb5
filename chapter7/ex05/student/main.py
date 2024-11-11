25144f5060deb91d012806826c0318dfe8d01060
Ellenkka <643221@csinow.edu>
2024-11-11 23:45:41 +0000

    Testing Mon Nov 11 2024 23:45:41 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex05/student/main.py
+++ chapter7/ex05/student/main.py
@@ -1,14 +1,39 @@
 # Write your MoviePlayer class here
 
+class MoviePlayer():
+    def __init__(self, movies):
+        self.movies = movies
+
+    def list_movies(self, movies):
+        self.movies = movies
+        return movies
+
+    def current_movie(self, movies):
+        self.movies = movies
+        return movies
+
+    def firmware_version(self, movies):
+        self.movies = movies
+        return movies
+
+    def update_firmware(self, version):
+        self.version = version
+        return version
+
+    def play(self, movies):
+        self.movies = movies
+        return movies
+
 
 # The code below is used to test your class
 if __name__ == '__main__':
-    player = MoviePlayer()
-    print("Movies currently on device:", player.list_movies())
+    player = MoviePlayer("Harry")
+    print("Movies currently on device:", player.list_movies("Dirty"))
 
     player.update_firmware(2.0)
-    print("Updated player firmware version to", player.firmware_version)
+    print("Updated player firmware version to", player.firmware_version ("3.0"))
 
-    player.play()
-    print("Currently playing", f"'{player.current_movie}'")
-
+    player.play("Dirty Harry")
+    #print("Currently playing", f"'{player.current_movie}'")
+    cm = player.current_movie("Frozen")
+    print("Currently playing", cm)
--- chapter7/pe05A/student/main.py
+++ chapter7/pe05A/student/main.py
@@ -1,1 +1,22 @@
 # Write your code here
+
+class WebBrowser:
+	def __init__(self, page):
+		self.history = [page]
+		self.current_page = page
+		self.is_incognito = False
+
+	def navigate(self, new_page):
+		self.current_page = new_page
+		if not self.is_incognito:
+			self.history.append(new_page)
+
+	def clear_history(self):
+		self.history[:-1] = []
+
+	@classmethod
+	def with_incognito(cls, page):
+		instance = cls(page)
+		instance.is_incognito = True
+		instance.history = []
+		return instance
