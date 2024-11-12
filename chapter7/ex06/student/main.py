4fa21bffc634dbf23b2b607275f751c8aeea071d
Ellenkka <643221@csinow.edu>
2024-11-12 02:16:21 +0000

    Testing Tue Nov 12 2024 02:16:21 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex06/student/main.py
+++ chapter7/ex06/student/main.py
@@ -1,3 +1,4 @@
+
 class Bird:
     def __init__(self, wingspan, lifespan, speed):
         self.wingspan = wingspan
@@ -11,9 +12,17 @@ class Bird:
             f"can fly at a maximum speed of {self.speed_in_mph}mph."
 
 # Write your Eagle class here
+class Eagle(Bird):
+    def __init__(self, wingspan, lifespan, speed, clutch_size=3):
+        super().__init__(wingspan, lifespan, speed)
+        self.clutch_size = clutch_size
 
-
-
+    def __str__(self):
+        return f"The {type(self).__name__.lower()} " \
+               f"has a wingspan up to {self.wingspan}ft," \
+               f"has a lifespan of {self.lifespan_in_years} years and " \
+               f"can fly at a maximum speed of {self.speed_in_mph} mph." \
+               f"has a clutch size of {self.clutch_size} cms."
 
 # This code is used to test your class
 if __name__ == '__main__':
