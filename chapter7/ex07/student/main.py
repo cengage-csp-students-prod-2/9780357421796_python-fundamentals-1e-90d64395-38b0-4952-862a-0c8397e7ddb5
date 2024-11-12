bffbe08a33e509d3709770922142c31e1ae2a3fc
Ellenkka <643221@csinow.edu>
2024-11-12 02:31:21 +0000

    Testing Tue Nov 12 2024 02:31:21 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex07/student/main.py
+++ chapter7/ex07/student/main.py
@@ -6,10 +6,32 @@
 
 # Write the BioTablet class here
 
-
-if __name__ == '__main__':
 # Write an instance of the BioTablet class
     
 # Call the scan_face method from the instance
 
 # Print the pixels from the instance
+
+class TabletCamera():
+    def __init__(self, pixels):
+        self.pixels = pixels
+        pass
+
+
+class Facial_recognition():
+    def __init__(self):
+        pass
+
+    def scan_face(self):
+        print("Scanning Face...")
+
+
+class BioTablet(TabletCamera, Facial_recognition):
+    def __init__(self, mp="12MP"):
+        self.mp = mp
+
+if __name__ == '__main__':
+    myBioTablet = BioTablet()
+    myFC = Facial_recognition()
+    myFC.scan_face()
+    print(myBioTablet.mp)
