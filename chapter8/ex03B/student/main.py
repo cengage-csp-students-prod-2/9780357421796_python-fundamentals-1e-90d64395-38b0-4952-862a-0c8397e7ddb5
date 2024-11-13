e0a16e73bee7e148ed8aabc38fc18f87c6a63a18
Ellenkka <643221@csinow.edu>
2024-11-13 01:30:17 +0000

    Testing Wed Nov 13 2024 01:30:17 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03B/student/main.py
+++ chapter8/ex03B/student/main.py
@@ -1,17 +1,24 @@
 # Write your function here
 
-import string
+import string  # Replace with any package you want to inspect
 
 def resources_scanner(package):
     """
     This function takes a package (module) as input and prints out the names
     of all the resources (attributes, methods, classes) defined in that package.
+    
     :param package: The package/module whose resources we want to list.
     """
+    # Get the list of resources in the package using dir()
     resources = dir(package)
+    
+    # Loop through the resources and print them
     for resource in resources:
         print(resource)
 
+# Example usage:
 if __name__ == '__main__':
-    import math
-    resources_scanner(math)
+    import string  # You can replace this with any module you are interested in.
+    
+    # Call the function with the string module (or any module/package)
+    resources_scanner(string)
