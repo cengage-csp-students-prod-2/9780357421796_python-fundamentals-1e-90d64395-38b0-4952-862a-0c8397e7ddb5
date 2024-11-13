886a5b602defe26595dab7a2902f6195c1b9c8b7
Ellenkka <643221@csinow.edu>
2024-11-13 01:28:32 +0000

    Testing Wed Nov 13 2024 01:28:32 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03B/student/main.py
+++ chapter8/ex03B/student/main.py
@@ -6,7 +6,6 @@ def resources_scanner(package):
     """
     This function takes a package (module) as input and prints out the names
     of all the resources (attributes, methods, classes) defined in that package.
-    
     :param package: The package/module whose resources we want to list.
     """
     resources = dir(package)
