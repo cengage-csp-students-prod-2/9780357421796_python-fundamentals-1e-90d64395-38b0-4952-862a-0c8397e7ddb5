ffbb93a78bc77adf63498bdc80472616926392a5
Ellenkka <643221@csinow.edu>
2024-11-13 01:24:31 +0000

    Testing Wed Nov 13 2024 01:24:31 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03B/student/main.py
+++ chapter8/ex03B/student/main.py
@@ -3,27 +3,26 @@
 import string 
 myStuff = []
 
-class methodPackage():
+class MethodPackage:
     def __init__(self, package):
         self.package = package
 
-    def resources_scanner(self, package):
-        self.package = package
+    def resources_scanner(self):
+        # Print the available resources in the package
         print(dir(self.package))
 
-    def print_resources_scanner(self, package):
-        self.package = package
-        for p in package:
-            myStuff.append(p)        
-            print (p, myStuff)
+    def print_resources_scanner(self):
+        # Iterate over the string or package and append to myStuff
+        for p in self.package:
+            myStuff.append(p)
+            print(p, myStuff)
 
-myPack = methodPackage('')
-myPack.resources_scanner(string)
-myPack.print_resources_scanner('')
-
+# Create an instance of MethodPackage with the string module
+myPack = MethodPackage(string)
+myPack.resources_scanner()  # Print resources of the string module
+myPack.print_resources_scanner()  # Process the string module
 
 if __name__ == '__main__':
-    import string 
-    myPack = methodPackage('')
-    myPack.resources_scanner(string)
-    myPack.print_resources_scanner('')
+    myPack = MethodPackage(string)
+    myPack.resources_scanner()  # Print resources of the string module
+    myPack.print_resources_scanner()  # Process the string module
