adaa3cde1998f76b21f4dab27a6c96bcd578405e
Ellenkka <643221@csinow.edu>
2024-11-13 01:18:14 +0000

    Testing Wed Nov 13 2024 01:18:14 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03B/student/main.py
+++ chapter8/ex03B/student/main.py
@@ -1,5 +1,31 @@
 # Write your function here
 
+import string 
+myStuff = []
+
+class methodPackage():
+    def __init__(self, package):
+        self.package = package
+
+    def resources_scanner (self, package):
+        self.package = package
+        print(dir(self.package))
+
+    def print_resources_scanner(self, package):
+        self.package = package
+        for p in package:
+            myStuff.append(p)        
+            print (p, myStuff)
+
+"""
+myPack = methodPackage('')
+myPack.resources_scanner(string)
+myPack.print_resources_scanner('')
+
+"""
+
 if __name__ == '__main__':
     import string 
-    resources_scanner(string)
+    myPack = methodPackage('')
+    myPack.resources_scanner(string)
+    myPack.print_resources_scanner('')
