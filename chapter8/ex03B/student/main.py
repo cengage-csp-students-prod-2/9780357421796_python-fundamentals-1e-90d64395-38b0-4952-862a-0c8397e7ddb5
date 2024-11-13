dda76054022d3f3efa94f4373858d75e3e64e3d8
Ellenkka <643221@csinow.edu>
2024-11-13 01:26:44 +0000

    Testing Wed Nov 13 2024 01:26:44 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03B/student/main.py
+++ chapter8/ex03B/student/main.py
@@ -1,28 +1,24 @@
 # Write your function here
 
-import string 
-myStuff = []
+import string  # Example import, replace with any module you want to inspect
 
-class MethodPackage:
-    def __init__(self, package):
-        self.package = package
+def resources_scanner(package):
+    """
+    This function takes a package (module) as input and prints out the names
+    of all the resources (attributes, methods, classes) defined in that package.
+    
+    :param package: The package/module whose resources we want to list.
+    """
+    # Get the list of resources in the package using dir()
+    resources = dir(package)
+    
+    # Loop through the resources and print them
+    for resource in resources:
+        print(resource)
 
-    def resources_scanner(self):
-        # Print the available resources in the package
-        print(dir(self.package))
-
-    def print_resources_scanner(self):
-        # Iterate over the string or package and append to myStuff
-        for p in self.package:
-            myStuff.append(p)
-            print(p, myStuff)
-
-# Create an instance of MethodPackage with the string module
-myPack = MethodPackage(string)
-myPack.resources_scanner()  # Print resources of the string module
-myPack.print_resources_scanner()  # Process the string module
-
+# Example usage:
 if __name__ == '__main__':
-    myPack = MethodPackage(string)
-    myPack.resources_scanner()  # Print resources of the string module
-    myPack.print_resources_scanner()  # Process the string module
+    import math  # You can replace this with any package/module you are interested in.
+    
+    # Call the function with the math module (or any module/package)
+    resources_scanner(math)
