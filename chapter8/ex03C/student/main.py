0716a9181f5b6d0d3309d5e6c9228c92f1624cd1
Ellenkka <643221@csinow.edu>
2024-11-13 01:44:57 +0000

    Testing Wed Nov 13 2024 01:44:57 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex03C/student/main.py
+++ chapter8/ex03C/student/main.py
@@ -1,6 +1,11 @@
 import random
 # Write your function here
 
+def number_randomizer(NumberOfTimes):
+    randos = []
+    for x in range(NumberOfTimes):
+        randos.append(random.randrange(1, 100))
+    return randos
 
 print(number_randomizer(2)) 
 print(number_randomizer(4))
