b567cf27507f2b0cd8ad5f9b5631e8de8674bf78
Ellenkka <643221@csinow.edu>
2024-11-14 01:57:41 +0000

    Testing Thu Nov 14 2024 01:57:41 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex06/student/main.py
+++ chapter8/ex06/student/main.py
@@ -1,5 +1,4 @@
 # Write your code here
 
-with open("newfile.txt", "w") as f:
-    data = f.write("I enjoy learning to code in Python")
-    print(data)
+with open('newfile.txt', 'w') as file:
+    file.write('I enjoy learning to code in Python')
