ab05c4b1ae92182f753d69d66c71bae29a23e06d
Ellenkka <643221@csinow.edu>
2024-11-14 02:14:20 +0000

    Testing Thu Nov 14 2024 02:14:20 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex07/student/main.py
+++ chapter8/ex07/student/main.py
@@ -1,1 +1,20 @@
 # Write your code here
+import csv
+
+with open('employees.csv', mode='r') as infile:
+    reader = csv.DictReader(infile)
+    
+    with open('wages.csv', mode='w', newline='') as outfile:
+        fieldnames = ['name', 'hours_worked', 'pay_rate']
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        
+        for row in reader:
+            name = row['name']
+            hours_worked = int(row['hours_worked'])  
+            pay_rate = hours_worked * 15
+            
+            writer.writerow({'name': name, 'hours_worked': hours_worked, 'pay_rate': pay_rate})
+
+print("wages.csv has been generated successfully.")
