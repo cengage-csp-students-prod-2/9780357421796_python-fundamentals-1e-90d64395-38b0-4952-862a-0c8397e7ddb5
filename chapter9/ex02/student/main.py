53bef2cd03ee9e8e1ee0fc43cdbfe0e3ad8e1a92
Ellenkka <643221@csinow.edu>
2024-11-19 02:28:31 +0000

    Testing Tue Nov 19 2024 02:28:31 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex02/student/main.py
+++ chapter9/ex02/student/main.py
@@ -2,15 +2,14 @@
 
 import string
 
-
 def createError():
     dinning = dict(
         name="Johnny's Restaurant",
         location="Times Square"
     )
-    print(f"I went to eat at {dinning['name']} yesterday in {dinning['address']}.")
+    print(f"I went to eat at {dinning['name']} yesterday in {dinning['location']}.")  # Fixed the key to 'location'
 
-    f = string.ascii_letterss
+    f = string.ascii_letters  # Corrected the typo
     print(f)
     return
 
@@ -18,3 +17,4 @@ def createError():
 # Use this code to test your function
 if __name__ == '__main__':
     createError()
+
