01ed364491af1d8ed73ab70b75cbaaa13a1ee91a
Ellenkka <643221@csinow.edu>
2024-11-19 02:34:36 +0000

    Testing Tue Nov 19 2024 02:34:36 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex02/student/main.py
+++ chapter9/ex02/student/main.py
@@ -2,14 +2,15 @@
 
 import string
 
+
 def createError():
     dinning = dict(
         name="Johnny's Restaurant",
         location="Times Square"
     )
-    print(f"I went to eat at {dinning['name']} yesterday in {dinning['location']}.")
+    print(f"I went to eat at {dinning['name']} yesterday in {dinning['address']}.")
 
-    f = string.ascii_letters
+    f = string.ascii_letterss
     print(f)
     return
 
@@ -17,4 +18,3 @@ def createError():
 # Use this code to test your function
 if __name__ == '__main__':
     createError()
-
