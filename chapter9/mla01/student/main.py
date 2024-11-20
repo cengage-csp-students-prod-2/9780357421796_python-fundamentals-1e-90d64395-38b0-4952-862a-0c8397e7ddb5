04965bb38ca26a52abdcc9cebb0713d023092490
Ellenkka <643221@csinow.edu>
2024-11-20 16:05:12 +0000

    Testing Wed Nov 20 2024 16:05:12 GMT+0000 (Coordinated Universal Time)

--- chapter9/mla01/student/main.py
+++ chapter9/mla01/student/main.py
@@ -1,7 +1,13 @@
 class EmailNotValidError():
-    """ Raised when the target email is not valid """
+""" Raised when the target email is not valid """
+ """
+    Custom exception for invalid email addresses.
 
-
+    Attributes:
+        message (str): Description of the error to help with debugging.
+"""
+    def __init__(self, message):
+        super().__init__(message)
 
 def is_email_valid(mailing_list):
     """
@@ -11,6 +17,23 @@ def is_email_valid(mailing_list):
        https://www.python.org/dev/peps/pep-0008/#documentation-strings.
 
    """
+   """
+    Validates an email address to ensure it contains an '@' symbol.
+
+    Args:
+        email (str): The email address to validate.
+
+    Raises:
+        EmailNotValidError: If the email address does not contain an '@' symbol.
+
+    Returns:
+        bool: True if the email is valid, False otherwise (though this is redundant since it raises an exception).
+    """
+
+    if '@' not in email:
+        raise EmailNotValidError(f"Invalid email address: {email}. Missing '@' symbol.")
+    return True
+    
 
 for key, email in  # Loop through the mailing list:
 
