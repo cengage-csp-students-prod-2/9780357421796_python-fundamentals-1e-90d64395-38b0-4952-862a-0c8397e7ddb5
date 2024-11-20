8f2ad5825b16f2b9eb781a852ca1f835f3d4835c
Ellenkka <643221@csinow.edu>
2024-11-20 15:45:24 +0000

    Testing Wed Nov 20 2024 15:45:24 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex03/student/main.py
+++ chapter9/ex03/student/main.py
@@ -2,6 +2,6 @@ import random
 
 
 try:
-    print(random.randinteger(5, 15))  # This is an error: incorrect method name
+    print(random.randinteger(5, 15))
 except AttributeError:
     print("Double check the attributes in your code and try again.")
--- chapter9/ex04/student/main.py
+++ chapter9/ex04/student/main.py
@@ -1,5 +1,17 @@
 # Write your class here
 
+class TimeEnteredNotValidError(Exception):
+    def __init__(self):
+        self.message = "The time is not valid"
+
+
+time_entered = 8
+
+try:
+    if time_entered < 9:
+        raise TimeEnteredNotValidError
+except TimeEnteredNotValidError as e:
+    print(e.message)
 
 # Test you custom exception class below
 
